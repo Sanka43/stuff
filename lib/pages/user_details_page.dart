dacee0a66ca3455a13fb66e3cad4344a83d33a34
sanka eranga <isankaerangahtc820@gmail.com>
2025-01-20 19:45:08 +0530

    1st commite

--- lib/pages/user_details_page.dart
+++ lib/pages/user_details_page.dart
@@ -6,8 +6,6 @@ class UserDetailsPage extends StatelessWidget {
   final String userId; // User ID passed to this page
   final CollectionReference usersCollection =
       FirebaseFirestore.instance.collection('users');
-  final CollectionReference itemsCollection =
-      FirebaseFirestore.instance.collection('items');
 
   UserDetailsPage({required this.userId});
 
@@ -24,12 +22,17 @@ class UserDetailsPage extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.transparent,
         elevation: 0,
-        leading: null, // This line removes the back arrow button
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
         actions: [
           IconButton(
             icon: const Icon(
               Icons.home,
-              color: Color.fromARGB(255, 255, 255, 255),
+              color: Colors.white,
             ),
             tooltip: 'Home',
             onPressed: () {
@@ -41,110 +44,164 @@ class UserDetailsPage extends StatelessWidget {
           ),
         ],
       ),
-      body: FutureBuilder<DocumentSnapshot>(
-        future: usersCollection.doc(userId).get(),
-        builder: (context, userSnapshot) {
-          if (userSnapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          }
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              'assets/background.png',
+              fit: BoxFit.cover,
+            ),
+          ),
+          FutureBuilder<DocumentSnapshot>(
+            future: usersCollection.doc(userId).get(),
+            builder: (context, userSnapshot) {
+              if (userSnapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              }
 
-          if (!userSnapshot.hasData || !userSnapshot.data!.exists) {
-            return const Center(
-              child: Text(
-                'User not found.',
-                style: TextStyle(
-                    fontSize: 18, color: Color.fromARGB(255, 0, 0, 0)),
-              ),
-            );
-          }
+              if (!userSnapshot.hasData || !userSnapshot.data!.exists) {
+                return const Center(
+                  child: Text(
+                    'User not found.',
+                    style: TextStyle(fontSize: 18, color: Colors.black),
+                  ),
+                );
+              }
 
-          final userData = userSnapshot.data!.data() as Map<String, dynamic>;
-          String username = userData['username'] ?? 'Unknown';
-          double totalCost = userData['totalCost']?.toDouble() ?? 0.0;
+              final userData =
+                  userSnapshot.data!.data() as Map<String, dynamic>;
+              String username = userData['username'] ?? 'Unknown';
+              double totalCost = userData['cost']?.toDouble() ?? 0.0;
 
-          return Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 100.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      '$username',
-                      style: const TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.deepPurpleAccent),
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      'Total Cost: Rs.${totalCost.toStringAsFixed(2)}',
-                      style: TextStyle(fontSize: 16, color: Colors.grey),
-                    ),
-                    SizedBox(height: 20),
-                    const Text(
-                      'Things Usage:',
-                      style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.deepPurpleAccent),
-                    ),
-                  ],
-                ),
-              ),
-              Expanded(
-                child: StreamBuilder<QuerySnapshot>(
-                  stream: itemsCollection
-                      .where('users', arrayContains: username)
-                      .snapshots(),
-                  builder: (context, itemSnapshot) {
-                    if (itemSnapshot.connectionState ==
-                        ConnectionState.waiting) {
-                      return Center(child: CircularProgressIndicator());
-                    }
-
-                    if (!itemSnapshot.hasData ||
-                        itemSnapshot.data!.docs.isEmpty) {
-                      return const Center(
-                        child: Text(
-                          'No usage found for this user.',
-                          style: TextStyle(fontSize: 16, color: Colors.grey),
-                        ),
-                      );
-                    }
-
-                    final items = itemSnapshot.data!.docs;
-
-                    return ListView.builder(
-                      itemCount: items.length,
-                      itemBuilder: (context, index) {
-                        final itemData =
-                            items[index].data() as Map<String, dynamic>;
-                        String materialType =
-                            itemData['materialType'] ?? 'Unknown';
-                        double cost = itemData['cost']?.toDouble() ?? 0.0;
-
-                        return Card(
-                          margin: EdgeInsets.all(10),
-                          elevation: 5,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
+              return Column(
+                children: [
+                  Center(
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 200.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Center(
+                            child: Text(
+                              '$username'.toUpperCase(),
+                              style: const TextStyle(
+                                  fontSize: 30,
+                                  letterSpacing: 2,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white),
+                            ),
                           ),
-                          child: ListTile(
-                            title: Text('Material: $materialType'),
-                            subtitle:
-                                Text('Cost: \$${cost.toStringAsFixed(2)}'),
+                          SizedBox(height: 30),
+                          const Center(
+                            child: Text(
+                              'Total Cost',
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  color: Color.fromARGB(255, 159, 159, 159)),
+                            ),
                           ),
-                        );
-                      },
-                    );
-                  },
-                ),
-              ),
-            ],
-          );
-        },
+                          SizedBox(height: 10),
+                          Center(
+                            child: Text(
+                              'Rs.${totalCost.toStringAsFixed(2)}',
+                              style: const TextStyle(
+                                  fontSize: 36,
+                                  color: Color.fromARGB(255, 255, 0, 0)),
+                            ),
+                          ),
+                          SizedBox(height: 20),
+                          Center(
+                            child: ElevatedButton(
+                              onPressed: () => _showReduceCostDialog(
+                                  context, totalCost, username),
+                              style: ElevatedButton.styleFrom(
+                                foregroundColor: Colors.white,
+                                backgroundColor:
+                                    Colors.transparent, // White text color
+                                side: const BorderSide(
+                                    color: Colors.white,
+                                    width: 2), // White border
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(
+                                      50), // Rounded corners
+                                ),
+                                elevation: 0, // No shadow
+                              ),
+                              child: const Padding(
+                                padding: EdgeInsets.all(8.0),
+                                child: Text(
+                                  'Reduce Cost',
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              );
+            },
+          ),
+        ],
       ),
+    );
+  }
+
+  void _showReduceCostDialog(
+      BuildContext context, double currentCost, String username) {
+    final TextEditingController costController = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Reduce Cost'),
+          content: TextField(
+            controller: costController,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+              labelText: 'Enter amount to reduce',
+              border: OutlineInputBorder(),
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('Cancel'),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                final String enteredCost = costController.text;
+                if (enteredCost.isNotEmpty) {
+                  double reduceAmount = double.tryParse(enteredCost) ?? 0.0;
+                  if (reduceAmount > 0 && reduceAmount <= currentCost) {
+                    await usersCollection.doc(userId).update({
+                      'cost': currentCost - reduceAmount,
+                    });
+                    Navigator.pop(context);
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text(
+                          reduceAmount > currentCost
+                              ? 'Reduce amount exceeds current cost.'
+                              : 'Enter a valid amount.',
+                        ),
+                      ),
+                    );
+                  }
+                }
+              },
+              child: const Text('Reduce'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
