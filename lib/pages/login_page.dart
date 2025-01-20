32171055dc20fdfea212e01eacbeb42a72530357
sanka eranga <isankaerangahtc820@gmail.com>
2025-01-21 00:34:45 +0530

    user dashboard

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -12,26 +12,47 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   final _usernameController = TextEditingController();
   final _passwordController = TextEditingController();
+  bool _isLoading = false;
 
   Future<void> loginUser() async {
-    String username = _usernameController.text;
-    String password = _passwordController.text;
+    setState(() {
+      _isLoading = true; // Start loading animation
+    });
 
-    var userSnapshot = await FirebaseFirestore.instance
-        .collection('users')
-        .where('username', isEqualTo: username)
-        .where('password', isEqualTo: password)
-        .get();
+    String username = _usernameController.text.trim();
+    String password = _passwordController.text.trim();
 
-    if (userSnapshot.docs.isNotEmpty) {
-      String role = userSnapshot.docs.first['role'];
-      if (role == 'admin') {
-        Navigator.pushReplacement(
-            context, MaterialPageRoute(builder: (context) => AdminDashboard()));
+    try {
+      var userSnapshot = await FirebaseFirestore.instance
+          .collection('users')
+          .where('username', isEqualTo: username)
+          .where('password', isEqualTo: password)
+          .get();
+
+      if (userSnapshot.docs.isNotEmpty) {
+        String role = userSnapshot.docs.first['role'];
+        if (role == 'admin') {
+          Navigator.pushReplacement(context,
+              MaterialPageRoute(builder: (context) => AdminDashboard()));
+        } else {
+          Navigator.pushReplacement(context,
+              MaterialPageRoute(builder: (context) => UserDashboard()));
+        }
       } else {
-        Navigator.pushReplacement(
-            context, MaterialPageRoute(builder: (context) => UserDashboard()));
+        // Show an error message if no user is found
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Invalid username or password')),
+        );
       }
+    } catch (e) {
+      // Handle any errors
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('An error occurred. Please try again.')),
+      );
+    } finally {
+      setState(() {
+        _isLoading = false; // Stop loading animation
+      });
     }
   }
 
@@ -54,8 +75,7 @@ class _LoginPageState extends State<LoginPage> {
                 child: BackdropFilter(
                   filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
                   child: Container(
-                    padding: const EdgeInsets.only(
-                        left: 10, top: 10, right: 10, bottom: 0),
+                    padding: const EdgeInsets.all(20),
                     decoration: BoxDecoration(
                       color: Colors.white.withOpacity(0.1),
                       borderRadius: BorderRadius.circular(15),
@@ -66,69 +86,78 @@ class _LoginPageState extends State<LoginPage> {
                         SizedBox(height: 20),
                         TextField(
                           controller: _usernameController,
-                          style: TextStyle(
-                              color: Colors.white), // Input text color
+                          style: TextStyle(color: Colors.white),
                           decoration: InputDecoration(
                             labelText: 'Username',
                             labelStyle: TextStyle(color: Colors.white),
-                            border: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                  color:
-                                      const Color.fromARGB(255, 255, 255, 255),
-                                  width: 2.0),
-                            ),
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                  color:
-                                      const Color.fromARGB(255, 253, 253, 253),
-                                  width: 2.0),
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                  color:
-                                      const Color.fromARGB(255, 255, 255, 255),
-                                  width: 2.0),
-                            ),
-                            filled: true,
-                            fillColor: const Color.fromARGB(0, 255, 255, 255),
+                            border: OutlineInputBorder(),
                           ),
                         ),
                         SizedBox(height: 10),
                         TextField(
                           controller: _passwordController,
-                          obscureText: true, 
-                          style: TextStyle(
-                              color: Colors.white), // Input text color
+                          obscureText: true,
+                          style: TextStyle(color: Colors.white),
                           decoration: InputDecoration(
                             labelText: 'Password',
                             labelStyle: TextStyle(color: Colors.white),
-                            border: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                  color:
-                                      const Color.fromARGB(255, 255, 255, 255),
-                                  width: 2.0),
-                            ),
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                  color:
-                                      const Color.fromARGB(255, 253, 253, 253),
-                                  width: 2.0),
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                  color:
-                                      const Color.fromARGB(255, 255, 255, 255),
-                                  width: 2.0),
-                            ),
-                            filled: true,
-                            fillColor: const Color.fromARGB(0, 255, 255, 255),
+                            border: OutlineInputBorder(),
                           ),
                         ),
                         SizedBox(height: 20),
-                        ElevatedButton(
-                          onPressed: loginUser,
-                          child: Text('Login'),
-                        ),
+                        _isLoading
+                            ? Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Container(
+                                    padding: EdgeInsets.symmetric(
+                                        vertical: 20, horizontal: 40),
+                                    decoration: BoxDecoration(
+                                      color: Colors.black.withOpacity(0.7),
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                    child: Column(
+                                      children: [
+                                        ShaderMask(
+                                          shaderCallback: (Rect bounds) {
+                                            return const LinearGradient(
+                                              colors: [
+                                                Color.fromARGB(255, 19, 175, 2),
+                                                Color.fromARGB(
+                                                    255, 255, 234, 0),
+                                                Color.fromARGB(
+                                                    255, 255, 68, 68),
+                                              ],
+                                              begin: Alignment.topLeft,
+                                              end: Alignment.bottomRight,
+                                            ).createShader(bounds);
+                                          },
+                                          child:
+                                              const CircularProgressIndicator(
+                                            valueColor:
+                                                AlwaysStoppedAnimation<Color>(
+                                                    Color.fromARGB(
+                                                        255, 255, 255, 255)),
+                                            strokeWidth: 4,
+                                          ),
+                                        ),
+                                        SizedBox(height: 20),
+                                        const Text(
+                                          'Logging in...',
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 16,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              )
+                            : ElevatedButton(
+                                onPressed: loginUser,
+                                child: Text('Login'),
+                              ),
                         TextButton(
                           onPressed: () {
                             Navigator.pushNamed(context, '/register');
